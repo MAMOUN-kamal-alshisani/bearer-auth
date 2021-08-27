ecfa0e4de98d77fd4275bc0e361294a592666052
mamoun kamal alshishani <mamoun.alshishani@yahoo.com>
2021-08-27 18:00:00 +0300

    32

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ db.sync()
          })
            .catch(console.error); 
            
+          //  db.sync()
+          //  .then(()=>{
+          //              server.start(process.env.PORT || 3002);
+           
+          //           })
+          //             .catch(console.error); 
+                      
+           
